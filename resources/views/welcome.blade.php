59ef6fcfd6b2fb6dd6f478061d7fddf5a9b4a5c0
Angel <angeldominguezlopez030206gmail.com>
2026-07-22 13:42:51 -0600

    All:Logo working for landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,13 @@
 
     <title>Rocket Papas</title>
 
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="icon" type="icon/x-icon" href="img/Rocke.png">
+    <link rel="icon" type="icon/x-icon" href="{{ asset('img/Rocke.png') }}">
 </head>
 
 <body class="antialiased">
